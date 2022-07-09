75e332a60ea68214b47bdea64f5f19aee4388903
outsider007 <nikolai.kuznetsov.nk@gmail.com>
2022-07-10 00:39:21 +0700

    add isGameOver test

---
+++ src/test/java/ru/kuznetsov/seabattle/server/logic/GameTest.java
@@ -0,0 +1,15 @@
+package ru.kuznetsov.seabattle.server.logic;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class GameTest {
+
+    @Test
+    public void isGameOver() {
+        assertTrue("Is not win points combination", Game.isGameOver(Arrays.asList(1,3,5,7)));
+    }
+}
